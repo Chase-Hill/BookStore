31b629ce809ca153419eff19675d549c1218a1cc
Chase <chaseaohill@gmail.com>
2023-02-09 18:05:34 -0500

    Created tableview controller and viewcell.

---
+++ BookStore/BookStore/CustomViewCell/BookListTabelViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookListTabelViewCell.swift
+//  BookStore
+//
+//  Created by Chase on 2/9/23.
+//
+
+import UIKit
+
+class BookListTabelViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
